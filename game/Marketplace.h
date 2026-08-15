88f2939448e5b6f328943857e8658849019f081d
Marco Böttcher <spacesinmotion@gmail.com>
2026-08-15 18:00:41 +0200

    marketplace no longer needs new day to reset

--- game/Marketplace.h
+++ game/Marketplace.h
@@ -38,10 +38,8 @@ float mp_render_order(Marketplace *mp) { return l_to_y(mp->display.location.y); 
 void mp_update(Marketplace *mp, GameScene *gs, Game *g, float dt) {
   (void)dt;
 
-  if (gs->a_new_day_just_started) {
+  if (gs->a_new_day_just_started)
     mp->manager_click_counter = 0;
-    wp_reduce_clicks(&mp->work_provider, mp->work_provider.clicks_done);
-  }
   bd_update(&mp->display, g);
 }
 
@@ -159,6 +157,7 @@ bool mp_deliver_resource_done(void *context, Wearisome *w, GameScene *gs) {
 
   Marketplace *mp = (Marketplace *)context;
   wp_done(&mp->work_provider, w);
+  wp_reduce_clicks(&mp->work_provider, 1);
   return false;
 }
 
